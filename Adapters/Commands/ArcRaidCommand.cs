fec0d3d0168655a94a5cd59864ba7eb161f0a683
Wesley <dusellfam@gmail.com>
2026-01-04 01:38:31 -0500

    Task 12

--- Adapters/Commands/ArcRaidCommand.cs
+++ Adapters/Commands/ArcRaidCommand.cs
@@ -1,4 +1,6 @@
+using Terraria;
 using Terraria.ModLoader;
+using TerrariaArcRaiders.Adapters.Players;
 using TerrariaArcRaiders.Adapters.Systems;
 
 namespace TerrariaArcRaiders.Adapters.Commands
@@ -12,7 +14,7 @@ namespace TerrariaArcRaiders.Adapters.Commands
 
         public override string Command => "arcraid";
 
-        public override string Description => "Dev raid control (stubbed).";
+        public override string Description => "Dev raid control (enter/exit/toggle).";
 
         public override string Usage => UsageText;
 
@@ -34,18 +36,62 @@ namespace TerrariaArcRaiders.Adapters.Commands
                 return;
             }
 
+            var player = caller.Player;
+            if (player is null)
+            {
+                caller.Reply("Player-only command.");
+                return;
+            }
+
+            var raidPlayer = player.GetModPlayer<RaidPlayer>();
+
             var verb = args[0].ToLowerInvariant();
             switch (verb)
             {
                 case "enter":
+                    HandleEnter(player);
+                    break;
                 case "exit":
+                    HandleExit(player);
+                    break;
                 case "toggle":
-                    caller.Reply($"Dev command ready: '{verb}' will be wired to raid interactions in the next task.");
+                    if (raidPlayer.IsInRaid)
+                    {
+                        HandleExit(player);
+                    }
+                    else
+                    {
+                        HandleEnter(player);
+                    }
                     break;
                 default:
                     caller.Reply($"Usage: {UsageText}");
                     break;
             }
+        }
+
+        private static void HandleEnter(Player player)
+        {
+            var entered = RaidSystem.TryInteractPortal(player);
+            if (!entered)
+            {
+                RaidUiNotifications.Send(player, RaidUiNotifications.AlreadyInRaid);
+                return;
+            }
+
+            RaidUiNotifications.Send(player, RaidUiNotifications.DevEnter);
+        }
+
+        private static void HandleExit(Player player)
+        {
+            var exited = RaidSystem.TryInteractExit(player);
+            if (!exited)
+            {
+                RaidUiNotifications.Send(player, RaidUiNotifications.NotInRaid);
+                return;
+            }
+
+            RaidUiNotifications.Send(player, RaidUiNotifications.DevExit);
         }
     }
 }
